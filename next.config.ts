7ebefb7cac8ccf2256f32b95c14e7b334ac35053
patrice jaszczynski <ppastg92@free.fr>
2025-02-10 17:03:06 +0100

    Suppression de index.txt

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
-import type { NextConfig } from "next";
+  import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  output: 'export',
-  images: { unoptimized: true },
-  basePath: "/qrcodestudio"
-};
+  const nextConfig: NextConfig = {
+    /* config options here */
+    output: 'export',
+    images: { unoptimized: true },
+    basePath: "/qrcodestudio"
+  };
 
-export default nextConfig;
+  export default nextConfig;
